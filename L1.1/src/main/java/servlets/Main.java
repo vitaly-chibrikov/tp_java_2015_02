ed8bf858e6d9836a54041a119e58b42d622cf2be
Vitaly <vitaly.chibrikov@gmail.com>
2015-02-13 19:36:53 +0300

    L1.1 example

---
+++ L1.1/src/main/java/servlets/Main.java
@@ -0,0 +1,24 @@
+package servlets;
+
+import frontend.Frontend;
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.servlet.ServletContextHandler;
+import org.eclipse.jetty.servlet.ServletHolder;
+
+/**
+ * @author v.chibrikov
+ */
+public class Main {
+    public static void main(String[] args) throws Exception {
+        Frontend frontend = new Frontend();
+
+        Server server = new Server(8080);
+        ServletContextHandler context = new ServletContextHandler(ServletContextHandler.SESSIONS);
+        server.setHandler(context);
+
+        context.addServlet(new ServletHolder(frontend), "/authform");
+
+        server.start();
+        server.join();
+    }
+}
